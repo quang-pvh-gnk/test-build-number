aa5f01d0d52dc2c117305f89e165b3b76b65f70e
quang-pvh-gnk <quang-pvh@genkisystem.com>
2023-10-19 18:33:54 +0700

    fdc

--- config_manager.py
+++ config_manager.py
@@ -9,6 +9,7 @@ import os
 
 PROJECT_ID = "medica-test-customer"
 CREDENTIALS = json.loads("{\"type\":\"service_account\",\"project_id\":\"medica-test-customer\",\"private_key_id\":\"5c3456c7940b1beb2d43b5f076a4316cc8fa7a8d\",\"private_key\":\"-----BEGINPRIVATEKEY-----\\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDOrOiDVI2BiR5n\\n8wkmZDGF0wa4HBm38HTuSykubBpGGZKyStsd/2za+vLkUSKFKtuSwgEcDi++y3HW\\nVx0V5rTnx2qndQAT1IBngi10dNgu9GUpa8PzuqKZpiHiepmFQxa1EUQehxoY2pw9\\nD4AtMd8r/3AArELq8pJIuYwf/J731KGV6uKrxe3F8dNBsDsypthLK8aLz7YoQWJd\\n9KtLyEozrHzGXxHQNIe4rsyTAL59P+PKZatsmGQ3ojiwII9kFvs13QB69HWYkKkq\\ngWV7XTaoFXG/uY5SH9LK2xI4ruSDDxlynF2BMXWjlA5Jav38T6zuBPIiprXxIB71\\nihkkWirXAgMBAAECggEAXX5sxJRoWzP7VEzmU765hG99N6ZGoCYfz3iizYRLsnB5\\nIzynuP9GcB1b6ZsmgZVstQwcUmO8h5QjEDJiTvkJ11wnzE8qVM4ptCl4hJki/sLC\\nTKWahFRmoBk+dOS/NECwMgL4jNLTJHEogprTgh5wdcfdFADZWM2ZDVsan5G5HCT9\\nQKym8hkGnFAp8grGLrA7Ea+TRGZdq1oLrAY7mUEncCRj6gK0nZk6VcUqxDK7yUtD\\nobSVSwwXVyJMzNwa1NFQT0v2irErCrjCef5PLzQ8S2BaByYQCH0kw5rVfjl6H+Am\\nkLvDLKESZAoS6J2WFDYWbnlcMEl22/6AIZrY//2RWQKBgQDobqgwNijaBrOsg5S5\\nz8zvxyIoeFSheYCdhm158Ok13Iau7ksq/rwnuYxrZ/XG3viE31bp1n1W7TNdPakP\\nIR3/mO2AHEDb8NgFROkcd+lajWsHSjvTsYc0TAdMwULw8jrF4LM/GUZhKw+Ir8Nq\\nQErYSBgbgSbdIoVbd8coOH+qxQKBgQDjoauUkUNNXXQ9BNsc3OKdR8BijkFoTkkz\\nYsI3Qc5qq4My9t+K58RYgpjNmAmPVXopgLMCjw5l/KTcVcy0Yfzvwrg9PrHBL9Xi\\nkkUCIiwMHiYMIpy6FBzjDOSH6ojHzQM12XliK2cK/04WcgHgEa0PQdtNwvINI7Lh\\nQ3wGmK1I6wKBgFi9Lq9BbjVXS6+4iMasJGUDFPJ4hm8j0UvS7+cXaCItMKqGxeID\\nYViVSZwUE8y6gg1Qq4EZZ78RToPxr6LcFPTpsPsxTj3qZL8WQR2iVlBBH7SnzdGT\\nvLflsv/F5UXSZUf48tZLUwh0BTk9SZE+PJT4aWPO2kShEmMKhtm/QFa1AoGAFiqy\\nOL+EnE4TebW1WLQx5TsNgwtzVyL4geaGHzdrUHvvRNFBp5c2SaA+HRdHJwwd31jH\\nFeK0dj4KF+LLEm3QYdDGfEBUswljfLLdwX1uaKeu8NOWC5DV1050GsA+HabbPy00\\nYtjzKeD+y7TCdf2Cqs+w+XuYcqsdSk52D3ZiuY0CgYBo+X02b6Y47tLkAfJWA71c\\nu7odv7SEOVTovhbm3cq95fyf3BrTqBfttcWoBunJX5j9EoyP3TA7o4ruMGmgXnCO\\nY4DmCAube8N7Pc5NsbWRfes/jOU8ytypYN9k7qxFeXD9YxEpqiLHpm+AvFL+QX5i\\ntKPwrA+dwG3mWR8GpiU+PQ==\\n-----ENDPRIVATEKEY-----\\n\",\"client_email\":\"firebase-adminsdk-ens19@medica-test-customer.iam.gserviceaccount.com\",\"client_id\":\"117174656220392763369\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_x509_cert_url\":\"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ens19%40medica-test-customer.iam.gserviceaccount.com\",\"universe_domain\":\"googleapis.com\"}")
+print(CREDENTIALS)
 
 BASE_URL = "https://firebaseremoteconfig.googleapis.com"
 REMOTE_CONFIG_ENDPOINT = "v1/projects/" + PROJECT_ID + "/remoteConfig"
@@ -38,7 +39,7 @@ def _get(save=False):
 
 
 
-    
+
     headers = {"Authorization": "Bearer " + _get_access_token()}
     resp = requests.get(REMOTE_CONFIG_URL, headers=headers)
 
