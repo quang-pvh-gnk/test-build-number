1df38f4b457f7c6c81f781e3b45053e014ba3d43
quang-pvh-gnk <quang-pvh@genkisystem.com>
2023-10-19 19:21:27 +0700

    .

--- config_manager.py
+++ config_manager.py
@@ -8,7 +8,21 @@ import json
 import os
 
 PROJECT_ID = "medica-test-customer"
-CREDENTIALS = json.loads("{\"type\":\"service_account\",\"project_id\":\"medica-test-customer\",\"private_key_id\":\"5c3456c7940b1beb2d43b5f076a4316cc8fa7a8d\",\"private_key\":\"-----BEGINPRIVATEKEY-----MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDOrOiDVI2BiR5n8wkmZDGF0wa4HBm38HTuSykubBpGGZKyStsd/2za+vLkUSKFKtuSwgEcDi++y3HWVx0V5rTnx2qndQAT1IBngi10dNgu9GUpa8PzuqKZpiHiepmFQxa1EUQehxoY2pw9D4AtMd8r/3AArELq8pJIuYwf/J731KGV6uKrxe3F8dNBsDsypthLK8aLz7YoQWJd9KtLyEozrHzGXxHQNIe4rsyTAL59P+PKZatsmGQ3ojiwII9kFvs13QB69HWYkKkqgWV7XTaoFXG/uY5SH9LK2xI4ruSDDxlynF2BMXWjlA5Jav38T6zuBPIiprXxIB71ihkkWirXAgMBAAECggEAXX5sxJRoWzP7VEzmU765hG99N6ZGoCYfz3iizYRLsnB5IzynuP9GcB1b6ZsmgZVstQwcUmO8h5QjEDJiTvkJ11wnzE8qVM4ptCl4hJki/sLCTKWahFRmoBk+dOS/NECwMgL4jNLTJHEogprTgh5wdcfdFADZWM2ZDVsan5G5HCT9QKym8hkGnFAp8grGLrA7Ea+TRGZdq1oLrAY7mUEncCRj6gK0nZk6VcUqxDK7yUtDobSVSwwXVyJMzNwa1NFQT0v2irErCrjCef5PLzQ8S2BaByYQCH0kw5rVfjl6H+AmkLvDLKESZAoS6J2WFDYWbnlcMEl22/6AIZrY//2RWQKBgQDobqgwNijaBrOsg5S5z8zvxyIoeFSheYCdhm158Ok13Iau7ksq/rwnuYxrZ/XG3viE31bp1n1W7TNdPakPIR3/mO2AHEDb8NgFROkcd+lajWsHSjvTsYc0TAdMwULw8jrF4LM/GUZhKw+Ir8NqQErYSBgbgSbdIoVbd8coOH+qxQKBgQDjoauUkUNNXXQ9BNsc3OKdR8BijkFoTkkzYsI3Qc5qq4My9t+K58RYgpjNmAmPVXopgLMCjw5l/KTcVcy0Yfzvwrg9PrHBL9XikkUCIiwMHiYMIpy6FBzjDOSH6ojHzQM12XliK2cK/04WcgHgEa0PQdtNwvINI7LhQ3wGmK1I6wKBgFi9Lq9BbjVXS6+4iMasJGUDFPJ4hm8j0UvS7+cXaCItMKqGxeIDYViVSZwUE8y6gg1Qq4EZZ78RToPxr6LcFPTpsPsxTj3qZL8WQR2iVlBBH7SnzdGTvLflsv/F5UXSZUf48tZLUwh0BTk9SZE+PJT4aWPO2kShEmMKhtm/QFa1AoGAFiqyOL+EnE4TebW1WLQx5TsNgwtzVyL4geaGHzdrUHvvRNFBp5c2SaA+HRdHJwwd31jHFeK0dj4KF+LLEm3QYdDGfEBUswljfLLdwX1uaKeu8NOWC5DV1050GsA+HabbPy00YtjzKeD+y7TCdf2Cqs+w+XuYcqsdSk52D3ZiuY0CgYBo+X02b6Y47tLkAfJWA71cu7odv7SEOVTovhbm3cq95fyf3BrTqBfttcWoBunJX5j9EoyP3TA7o4ruMGmgXnCOY4DmCAube8N7Pc5NsbWRfes/jOU8ytypYN9k7qxFeXD9YxEpqiLHpm+AvFL+QX5itKPwrA+dwG3mWR8GpiU+PQ==-----ENDPRIVATEKEY-----\",\"client_email\":\"firebase-adminsdk-ens19@medica-test-customer.iam.gserviceaccount.com\",\"client_id\":\"117174656220392763369\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_x509_cert_url\":\"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ens19%40medica-test-customer.iam.gserviceaccount.com\",\"universe_domain\":\"googleapis.com\"}")
+CREDENTIALS = json.loads('''
+{
+  "type": "service_account",
+  "project_id": "medica-test-customer",
+  "private_key_id": "5c3456c7940b1beb2d43b5f076a4316cc8fa7a8d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDOrOiDVI2BiR5n\n8wkmZDGF0wa4HBm38HTuSykubBpGGZKyStsd/2za+vLkUSKFKtuSwgEcDi++y3HW\nVx0V5rTnx2qndQAT1IBngi10dNgu9GUpa8PzuqKZpiHiepmFQxa1EUQehxoY2pw9\nD4AtMd8r/3AArELq8pJIuYwf/J731KGV6uKrxe3F8dNBsDsypthLK8aLz7YoQWJd\n9KtLyEozrHzGXxHQNIe4rsyTAL59P+PKZatsmGQ3ojiwII9kFvs13QB69HWYkKkq\ngWV7XTaoFXG/uY5SH9LK2xI4ruSDDxlynF2BMXWjlA5Jav38T6zuBPIiprXxIB71\nihkkWirXAgMBAAECggEAXX5sxJRoWzP7VEzmU765hG99N6ZGoCYfz3iizYRLsnB5\nIzynuP9GcB1b6ZsmgZVstQwcUmO8h5QjEDJiTvkJ11wnzE8qVM4ptCl4hJki/sLC\nTKWahFRmoBk+dOS/NECwMgL4jNLTJHEogprTgh5wdcfdFADZWM2ZDVsan5G5HCT9\nQKym8hkGnFAp8grGLrA7Ea+TRGZdq1oLrAY7mUEncCRj6gK0nZk6VcUqxDK7yUtD\nobSVSwwXVyJMzNwa1NFQT0v2irErCrjCef5PLzQ8S2BaByYQCH0kw5rVfjl6H+Am\nkLvDLKESZAoS6J2WFDYWbnlcMEl22/6AIZrY//2RWQKBgQDobqgwNijaBrOsg5S5\nz8zvxyIoeFSheYCdhm158Ok13Iau7ksq/rwnuYxrZ/XG3viE31bp1n1W7TNdPakP\nIR3/mO2AHEDb8NgFROkcd+lajWsHSjvTsYc0TAdMwULw8jrF4LM/GUZhKw+Ir8Nq\nQErYSBgbgSbdIoVbd8coOH+qxQKBgQDjoauUkUNNXXQ9BNsc3OKdR8BijkFoTkkz\nYsI3Qc5qq4My9t+K58RYgpjNmAmPVXopgLMCjw5l/KTcVcy0Yfzvwrg9PrHBL9Xi\nkkUCIiwMHiYMIpy6FBzjDOSH6ojHzQM12XliK2cK/04WcgHgEa0PQdtNwvINI7Lh\nQ3wGmK1I6wKBgFi9Lq9BbjVXS6+4iMasJGUDFPJ4hm8j0UvS7+cXaCItMKqGxeID\nYViVSZwUE8y6gg1Qq4EZZ78RToPxr6LcFPTpsPsxTj3qZL8WQR2iVlBBH7SnzdGT\nvLflsv/F5UXSZUf48tZLUwh0BTk9SZE+PJT4aWPO2kShEmMKhtm/QFa1AoGAFiqy\nOL+EnE4TebW1WLQx5TsNgwtzVyL4geaGHzdrUHvvRNFBp5c2SaA+HRdHJwwd31jH\nFeK0dj4KF+LLEm3QYdDGfEBUswljfLLdwX1uaKeu8NOWC5DV1050GsA+HabbPy00\nYtjzKeD+y7TCdf2Cqs+w+XuYcqsdSk52D3ZiuY0CgYBo+X02b6Y47tLkAfJWA71c\nu7odv7SEOVTovhbm3cq95fyf3BrTqBfttcWoBunJX5j9EoyP3TA7o4ruMGmgXnCO\nY4DmCAube8N7Pc5NsbWRfes/jOU8ytypYN9k7qxFeXD9YxEpqiLHpm+AvFL+QX5i\ntKPwrA+dwG3mWR8GpiU+PQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-ens19@medica-test-customer.iam.gserviceaccount.com",
+  "client_id": "117174656220392763369",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ens19%40medica-test-customer.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+''')
 
 BASE_URL = "https://firebaseremoteconfig.googleapis.com"
 REMOTE_CONFIG_ENDPOINT = "v1/projects/" + PROJECT_ID + "/remoteConfig"
@@ -21,11 +35,7 @@ def _get_access_token():
     :return: Access token.
     """
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(CREDENTIALS, SCOPES)
-    print("kokokok")
-    print(credentials)
     access_token_info = credentials.get_access_token()
-    print("pqpqpqpqpqpq")
-    print(access_token_info)
     return access_token_info.access_token
 
 
@@ -37,12 +47,6 @@ def _get(save=False):
     Retrieve the current Firebase Remote Config template from server and store it
     locally.
     """
-
-
-
-
-
-
     headers = {"Authorization": "Bearer " + _get_access_token()}
     resp = requests.get(REMOTE_CONFIG_URL, headers=headers)
 
